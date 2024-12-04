eade5bd56f56314a8d3822bd0e74f86a681606fd
DmrHdxt <aa6132788@gmail.com>
2024-12-04 14:44:18 -0400

    fix: allow insecure requests

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -21,6 +21,15 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests',
+          },
+        ],
+      },
+      {
         source: '/fonts/:path*',
         headers: [
           {
